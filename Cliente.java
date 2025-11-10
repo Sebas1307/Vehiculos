15b703c0231853db77e592d6f3f141918380b609
Sebas <sebastianpa138@gmail.com>
2025-11-09 21:36:08 -0500

    Primer commit

---
+++ Cliente.java
@@ -0,0 +1,60 @@
+public class Cliente {
+   String Cedula;
+   String Nombre;
+   String Apellido;
+   String Telefono;
+   String Direccion;
+   String LicenciaConduccion;
+
+   public Cliente() {
+
+   }
+
+   public void getCedula() {
+      System.out.println(Cedula);
+   }
+
+   public void getNombre() {
+      System.out.println(Nombre);
+   }
+
+   public void getApellido() {
+      System.out.println(Apellido);
+   }
+
+   public void getTelefono() {
+      System.out.println(Telefono);
+   }
+
+   public void getDireccion() {
+      System.out.println(Direccion);
+   }
+
+   public void getLicenciaConduccion() {
+      System.out.println(LicenciaConduccion);
+   }
+
+   public void setCedula() {
+
+   }
+
+   public void setNombre() {
+
+   }
+
+   public void setApellido() {
+
+   }
+
+   public void setTelefono() {
+
+   }
+
+   public void setDireccion() {
+
+   }
+
+   public void setLicenciaConduccion() {
+
+   }
+}
