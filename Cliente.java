85a9828f769c668d29dc1931c2a6d20e146c97a9
Sebastian Patiño Caro <106630623+Sebas1307@users.noreply.github.com>
2025-11-11 15:07:38 -0500

    Refactor Cliente class to use private fields and getters/setters

--- Cliente.java
+++ Cliente.java
@@ -1,57 +1,71 @@
 public class Cliente {
-   String Cedula;
-   String Nombre;
-   String Apellido;
-   String Telefono;
-   String Direccion;
-   String LicenciaConduccion;
+   private String Cedula;
+   private String Nombre;
+   private String Apellido;
+   private String Telefono;
+   private String Direccion;
+   private String LicenciaConduccion;
 
-   public Cliente() {
+   private Metodos metodos = new Metodos();
 
-   }
-
-   public void getCedula() {
-      System.out.println(Cedula);
-   }
-
-   public void getNombre() {
-      System.out.println(Nombre);
-   }
-
-   public void getApellido() {
-      System.out.println(Apellido);
-   }
-
-   public void getTelefono() {
-      System.out.println(Telefono);
-   }
-
-   public void getDireccion() {
-      System.out.println(Direccion);
-   }
-
-   public void getLicenciaConduccion() {
-      System.out.println(LicenciaConduccion);
+   public String getCedula() {
+      return Cedula;
    }
 
    public void setCedula(String cedula) {
-      Cedula = cedula;
+      if(metodos.isDigit(cedula)) {
+         Cedula = cedula;
+      } else {
+         System.out.println("Hubo un error al digitar la cedula. Asegurate de solo digitar numeros.");
+      }
+   }
+
+   public String getNombre() {
+      return Nombre;
    }
 
    public void setNombre(String nombre) {
-      Nombre = nombre;
+      if(metodos.isLetter(nombre)) {
+         Nombre = nombre;
+      } else {
+         System.out.println("Hubo un error al digitar el nombre. Asegurate de solo digitar letras.");
+      }
+   }
+
+   public String getApellido() {
+      return Apellido;
    }
 
    public void setApellido(String apellido) {
-      Apellido = apellido;
+      if(metodos.isLetter(apellido)) {
+         Apellido = apellido;
+      } else {
+         System.out.println("Hubo un error al digitar el apellido. Asegurate de solo digitar letras.");
+      }
+   }
+
+   public String getTelefono() {
+      return Telefono;
    }
 
    public void setTelefono(String telefono) {
-      Telefono = telefono;
+      if(metodos.isPhoneNumber(telefono)) {
+         Telefono = telefono;
+      } else {
+         System.out.println("Hubo un error al digitar el telefono. Asegurate de solo digitar numeros y tener 10 digitos.");
+      }
+   }
+
+   public String getDireccion() {
+      return Direccion;
    }
 
    public void setDireccion(String direccion) {
       Direccion = direccion;
+   }
+
+   public String getLicenciaConduccion() {
+      return LicenciaConduccion;
    }
 
    public void setLicenciaConduccion(String licenciaConduccion) {
