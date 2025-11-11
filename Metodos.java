2f23cc35421a97e05ec1f923b25695ad183fe195
Sebastian Patiño Caro <106630623+Sebas1307@users.noreply.github.com>
2025-11-11 15:07:57 -0500

    Add validation methods for digits, letters, and phone numbers

--- Metodos.java
+++ Metodos.java
@@ -1,7 +1,38 @@
 public class Metodos {
-   public void validarCedula(String cedula){
-      for (char letra : cedula.toCharArray()) {
-         System.out.println(letra);
+   
+   public boolean isDigit(String numbers) {
+      for (char number : numbers.toCharArray()) {
+         boolean isDigit = Character.isDigit(number);
+         if (!isDigit) {
+            // System.out.println("Hubo un error. Asegurate de solo digitar numeros.");
+            return false;
+         }
+      }
+      return true;
+   }
+
+   public boolean isLetter(String letters) {
+      for (char letter : letters.toCharArray()) {
+         boolean isLetter = Character.isLetter(letter);
+         if (!isLetter) {
+            // System.out.println("Hubo un error. Asegurate de solo digitar letras.");
+            return false;
+         }
+      }
+      return true;
+   }
+
+   private int VALID_PHONE_LENGTH = 10;
+   
+   public boolean isPhoneNumber(String phone) {
+      boolean isDigit = isDigit(phone);
+      int phoneLength = phone.length();
+      boolean isLengthValid = phoneLength == VALID_PHONE_LENGTH;
+
+      if(isDigit && isLengthValid) {
+         return true;
+      } else {
+         return false;
       }
    }
 }
