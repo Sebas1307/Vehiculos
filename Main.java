7ed16e89adb4c5c96e57e4405050dc4ffef5d1df
Branqc <quinterobran7@gmail.com>
2025-11-12 19:49:19 -0500

    validacion general y metodos

--- Main.java
+++ Main.java
@@ -1,14 +1,48 @@
+import java.util.Scanner;
+import java.util.LinkedList;
+
+
 public class Main {
 public static void main(String[] args) {
-		Metodos met = new Metodos();
-		met.Validarplaca();
-        met.ValidarMarca();
-        met.ValidarModelo();
-        met.ValidarPrecioDiario();
-        met.ValidarEstado();
-        met.ValidarTipoCombustible();
-        met.ValidarTransmicion();
-        met.ValidarTraccion();
-        met.ValidarCapacidadMaletero();
+    Scanner sc = new Scanner(System.in);
+    int opcion = 0;
+    LinkedList<CarroSedan> listaSedan = new LinkedList<>();
+    LinkedList<CamionetaSUV> listaSUV = new LinkedList<>();
+    Metodos m = new Metodos();
+	Validaciones v = new Validaciones();
+    boolean salir = true;
+    while (salir) {
+        System.out.println("----- Menu de Vehiculos -----");
+        System.out.println("1. Registrar Carro Sedan");
+        System.out.println("2. Mostrar Carros Sedan");
+        System.out.println("3. Registrar Camioneta SUV");
+        System.out.println("4. Mostrar Camionetas SUV");
+        System.out.println("5. Salir");
+        System.out.print("Seleccione una opcion: ");
+        opcion = v.ValidarEntero(sc);
+
+        switch (opcion) {
+            case 1:
+                CarroSedan sedan = new CarroSedan("","",0,0,"","","");
+                listaSedan.add(m.RegistrarSedan(sedan));
+                break;
+            case 2:
+                m.MostrarSedan(listaSedan);
+                break;
+            case 3:
+                CamionetaSUV suv = new CamionetaSUV("","",0,0,"","",0);
+                listaSUV.add(m.RegistrarSUV(suv));
+                break;
+            case 4:
+                m.MostrarSUV(listaSUV);
+                break;
+            case 5:
+                salir = false;
+                System.out.println("Saliendo del programa...");
+                break;
+            default:
+                System.out.println("Opcion invalida. Por favor, intente de nuevo.");
+        }
+    }
 	}
 }
